9f9f98549f692351b34dfda00d01bf8619879f81
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-11-05 12:39:25 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Solution {
 
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
-        int sizeForeachWord = dictionary[0].length();
+        int sizeForEachWord = dictionary[0].length();
         List<String> wordsWithinTwoEdits = new ArrayList<>();
 
         for (String wordOne : queries) {
             for (String wordTwo : dictionary) {
-                if (wordsAreWithinTwoEdits(wordOne, wordTwo, sizeForeachWord)) {
+                if (wordsAreWithinTwoEdits(wordOne, wordTwo, sizeForEachWord)) {
                     wordsWithinTwoEdits.add(wordOne);
                     break;
                 }
@@ -19,9 +19,9 @@ public class Solution {
         return wordsWithinTwoEdits;
     }
 
-    private boolean wordsAreWithinTwoEdits(String first, String second, int sizeForeachWord) {
+    private boolean wordsAreWithinTwoEdits(String first, String second, int sizeForEachWord) {
         int countEdits = 0;
-        for (int i = 0; i < sizeForeachWord; ++i) {
+        for (int i = 0; i < sizeForEachWord; ++i) {
             if (first.charAt(i) != second.charAt(i) && ++countEdits > 2) {
                 return false;
             }
