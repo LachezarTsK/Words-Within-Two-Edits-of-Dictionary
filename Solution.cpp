18816412ad2f4329338ec96078e636fbce0a6c87
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-11-05 12:40:35 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -7,12 +7,12 @@ class Solution {
     
 public:
     vector<string> twoEditWords(const vector<string>& queries, const vector<string>& dictionary) const {
-        size_t sizeForeachWord = dictionary[0].length();
+        size_t sizeForEachWord = dictionary[0].length();
         vector<string> wordsWithinTwoEdits;
 
         for (const auto& wordOne : queries) {
             for (const auto& wordTwo : dictionary) {
-                if (wordsAreWithinTwoEdits(wordOne, wordTwo, sizeForeachWord)) {
+                if (wordsAreWithinTwoEdits(wordOne, wordTwo, sizeForEachWord)) {
                     wordsWithinTwoEdits.push_back(wordOne);
                     break;
                 }
@@ -23,9 +23,9 @@ public:
     
 private:
     //Alternatively, C++20: ...(string_view first, string_view second, ...)
-    bool wordsAreWithinTwoEdits(const string& first, const string& second, size_t sizeForeachWord) const {
+    bool wordsAreWithinTwoEdits(const string& first, const string& second, size_t sizeForEachWord) const {
         int countEdits = 0;
-        for (size_t i = 0; i < sizeForeachWord; ++i) {
+        for (size_t i = 0; i < sizeForEachWord; ++i) {
             if (first[i] != second[i] && ++countEdits > 2) {
                 return false;
             }
